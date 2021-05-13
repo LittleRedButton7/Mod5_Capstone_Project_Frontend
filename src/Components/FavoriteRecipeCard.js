47e20bf64ac9c745a4aa9fe15e83e6f85499688d
LittleRedButton <ritz1484@cs.com>
2021-05-12 20:21:36 -0600

    Added card flip and moved buttons.

--- src/Components/FavoriteRecipeCard.js
+++ src/Components/FavoriteRecipeCard.js
@@ -15,11 +15,11 @@ export default function RecipeCard({recipe, type}) {
     const favoriteButtonDisabled = storedRecipe ? true : false
 
     return (
-        <div className="recipe-card">
-            <img src={recipe.image} alt="recipe.name" className="recipe-image"></img>
-            <h1 className="recipe-title">{recipe.label}</h1>
-            <button className="recipe-url"><a href={recipe.url}>See Full Recipe</a></button>
-            <ul className="nutrient-list">
+        <div className="favorite-recipe-card">
+            <img src={recipe.image} alt="recipe.name" className="favorite-recipe-image"></img>
+            <h1 className="favorite-recipe-title">{recipe.label}</h1>
+            <button className="favorite-recipe-url"><a href={recipe.url}>See Full Recipe</a></button>
+            <ul className="favorite-nutrient-list">
                 <ul>Recipe Calories: {recipe.calories.toFixed(0)} cals</ul>
                 <ul>Recipe Total Fat: {recipe.totalNutrients.FAT.quantity.toFixed(0)} g</ul>
                 <ul>Recipe Total Carbs: {recipe.totalNutrients.CHOCDF.quantity.toFixed(0)} g</ul>
